4cb93ad02715f67867a7325cae181310090ba28c
Nan Andrea <balajelandrea@yahoo.com>
2023-03-06 09:29:12 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contact">
+      <img src="IMG.jpg" alt="poza profil"></img>
+      <h3>CONTACT</h3>
+        <ul>
+          <li>Phone
+            <p>+40756 87 55 00</p>
+          </li>
+          <li>Email 
+           <p>balajelandrea@yahoo.com</p> 
+          </li>
+          <li>Address
+            <p>Cluj-Napoca, jud Cluj</p>
+          </li>
+        </ul>
+    </div>
+    
+    <div className='about'>
+      <h1>NAN ANDREA</h1>
+      <div>
+        <h3>ABOUT ME</h3>
+        <p>Sunt o persoana energica, cu o gandire analitica, axata pe performanta.<br/>
+        Cele mai mari calitati ale mele sunt seriozitatea si perseverenta.<br/>
+        Sunt o persoana care se adapteaza foarte repede in colective noi si imi place munca in echipa.
+        
+          </p>
+        <h3>EDUCATION</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+          <h3>WORK EXPERIENCE</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+      </div>
+    </div>
     </div>
   );
 }
